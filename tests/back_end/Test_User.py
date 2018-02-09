0528918d6259a35b0205a326e6fbd5dde1555353
Ross Piper <rdpiper@uchicago.edu>
2018-02-09 15:34:21 -0600

    Changes to User and Venue tests

--- tests/back_end/Test_User.py
+++ tests/back_end/Test_User.py
@@ -9,26 +9,31 @@ user5 = User("Ross", "Piper", "") # failure
 
 
 def test_validuser:
-    assert user1.fname == "Ethan" and
+    assert user1.id != None and
+           user1.fname == "Ethan" and
            user1.lname == "Reeder" and
            user1.email_address == "er@example.com"
 
 def test_dupemail:
-    assert user2.fname == None and
+    assert user2.id == None and
+           user2.fname == None and
            user2.lname == None and
            user2.email_address == None
 
 def test_badfname:
-    assert user3.fname == None and
+    assert user2.id == None and
+           user3.fname == None and
            user3.lname == None and
            user3.email_address == None
 
 def test_badlname:
-    assert user4.fname == None and
+    assert user2.id == None and
+           user4.fname == None and
            user4.lname == None and
            user4.email_address == None
 
 def test_noemail:
-    assert user5.fname == None and
+    assert user2.id == None and
+           user5.fname == None and
            user5.lname == None and
            user5.email_address == None
---
+++ tests/back_end/Test_Venue.py
@@ -0,0 +1,4 @@
+from ../../back_end/Admit01_Blockchain.py import *
+
+venue1 = Venue("Apollo Theater", "Chicago, IL") # success
+venue2 = Venue()
