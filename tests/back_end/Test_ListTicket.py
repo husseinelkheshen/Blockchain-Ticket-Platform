e8691f45abafb6d29121eab9726673f6b461f6b2
Ross Piper <rdpiper@uchicago.edu>
2018-02-09 15:46:01 -0600

    Added tweaks/comments

--- tests/back_end/Test_ListTicket.py
+++ tests/back_end/Test_ListTicket.py
@@ -8,19 +8,35 @@ seat1 = Seat("Orchestra Center", "B", 5)
 ticket1 = Ticket(event1, 150, seat1)
 user1 = User("Ethan", "Reeder", "er@example.com")
 
+# user1 deposits enough money to buy ticket1
+user1.wallet = 200
+# user1 buys ticket1
+user1.buyTicket(ticket1)
+
 def test_allvalid:
+    #
+    # A Ticket for a future-dated Event should list successfully by its owner
+    # at any non-negative list price
+    #
     list_price = 200
     ticket1.listTicket(list_price, user1.getID())
     assert ticket1.isForSale() and
            ticket1.list_price == list_price
 
 def test_badlistprice:
+    #
+    # List prices must be non-negative
+    #
     list_price = -50
     ticket1.listTicket(list_price, user1.getID())
     assert !(ticket1.isForSale() or
              ticket1.list_price == list_price)
 
 def test_baduser:
+    #
+    # A Ticket can not be listed by a User who does not exist and/or does not
+    # currently own that Ticket
+    #
     list_price = 200
     ticket1.listTicket(list_price, user1.getID() + 1)
     assert !(ticket1.isForSale() or
