b087d6363aaf41a6dcbd08770e8817d1b4469fc6
Ross Piper <rdpiper@uchicago.edu>
2018-02-13 14:56:52 -0600

    More import and documentation fixes

--- tests/back_end/Test_Event.py
+++ tests/back_end/Test_Event.py
@@ -1,4 +1,4 @@
-from ../../back_end/Admit01_Blockchain.py import *
+from back_end.Admit01_Blockchain import *
 from datetime import datetime
 from datetime import timedelta
 
@@ -13,26 +13,20 @@ event1 = Event("Lady Gaga", valid_datetime, "Stadium world tour") # success
 event2 = Event("", valid_datetime, "Marlins vs. Red Sox") # failure
 event3 = Event("Hamilton", invalid_datetime, "2016 Tony winner") # failure
 
-def test_allvalid:
-    #
-    # Future-dated Event with non-empty name should construct successfully
-    #
-    assert event1.name == "Lady Gaga" and
-           event1.datetime == valid_datetime and
-           event1.desc == "Stadium world tour"
+def test_allvalid():
+    """ Future-dated Event with non-empty name should construct successfully """
+    assert (event1.name == "Lady Gaga" and
+            event1.datetime == valid_datetime and
+            event1.desc == "Stadium world tour")
 
-def test_badname:
-    #
-    # Nameless Event should not construct
-    #
-    assert event2.name == None and
-           event2.datetime == None and
-           event2.desc == None 
+def test_badname():
+    """ Nameless Event should not construct """
+    assert (event2.name == None and
+            event2.datetime == None and
+            event2.desc == None)
 
-def test_baddatetime:
-    #
-    # Past-dated Event should not construct
-    #
-    assert event3.name == None and
-           event3.datetime == None and
-           event3.desc == None
+def test_baddatetime():
+    """ Past-dated Event should not construct """
+    assert (event3.name == None and
+            event3.datetime == None and
+            event3.desc == None)
--- tests/back_end/Test_ListTicket.py
+++ tests/back_end/Test_ListTicket.py
@@ -1,46 +1,42 @@
-from ../../back_end/Admit01_Blockchain.py import *
+from back_end.Admit01_Blockchain import *
 from datetime import datetime
 from datetime import timedelta
 
-valid_datetime = datetime.now() + timedelta(days=7) # one week from now
+valid_datetime = datetime.now() + timedelta(days=7)    # one week from now
 event1 = Event("Test Event", valid_datetime, "Test description")
 seat1 = Seat("Orchestra Center", "B", 5)
 ticket1 = Ticket(event1, 150, seat1)
 user1 = User("Ethan", "Reeder", "er@example.com")
 
-# user1 deposits enough money to buy ticket1
-user1.wallet = 200
-# user1 buys ticket1
-user1.buyTicket(ticket1)
+user1.wallet = 200    # user1 deposits enough money to buy ticket1
+user1.buyTicket(ticket1)    # user1 buys ticket1
 
-def test_allvalid:
-    #
-    # A Ticket for a future-dated Event should list successfully by its owner
-    # at any non-negative list price
-    #
+def test_allvalid():
+    """
+    A Ticket for a future-dated Event should list successfully by its owner
+    at any non-negative list price
+    """
     ticket1.for_sale = False
     list_price = 200
     ticket1.listTicket(list_price, user1.getID())
-    assert ticket1.isForSale() and
-           ticket1.list_price == list_price
+    assert (ticket1.isForSale() and
+            ticket1.list_price == list_price)
 
-def test_badlistprice:
-    #
-    # List prices must be non-negative
-    #
+def test_badlistprice():
+    """ List prices must be non-negative """
     ticket1.for_sale = False
     list_price = -50
     ticket1.listTicket(list_price, user1.getID())
-    assert !(ticket1.isForSale() or
-             ticket1.list_price == list_price)
+    assert not (ticket1.isForSale() or
+                ticket1.list_price == list_price)
 
-def test_baduser:
-    #
-    # A Ticket can not be listed by a User who does not exist and/or does not
-    # currently own that Ticket
-    #
+def test_baduser():
+    """
+    A Ticket can not be listed by a User who does not exist and/or does not
+    currently own that Ticket
+    """
     ticket1.for_sale = False
     list_price = 200
     ticket1.listTicket(list_price, user1.getID() + 1)
-    assert !(ticket1.isForSale() or
-             ticket1.list_price == list_price)
+    assert not (ticket1.isForSale() or
+                ticket1.list_price == list_price)
