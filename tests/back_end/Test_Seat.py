4cd6ed97044b2c8f4b522bd9f832030a1e9e723c
Ross Piper <rdpiper@uchicago.edu>
2018-02-13 14:48:45 -0600

    Minor changes to imports, etc. so that tests run

--- tests/back_end/Test_Seat.py
+++ tests/back_end/Test_Seat.py
@@ -1,4 +1,4 @@
-from ../../back_end/Admit01_Blockchain.py import *
+from back_end.Admit01_Blockchain import *
 
 valid_section = "Mezzanine"
 valid_row = "B"
@@ -9,34 +9,34 @@ seat2 = Seat("", valid_row, valid_seatno) # failure
 seat3 = Seat(valid_section, "", valid_seatno) # failure
 seat4 = Seat(valid_section, valid_row, -3) # failure
 
-def test_allvalid:
+def test_allvalid():
     #
     # A Seat with valid section, row, and seat_no should construct successfully
     #
-    assert seat1.section == valid_section and
-           seat1.row == valid_row and
-           seat1.seat_no == valid_seatno
+    assert (seat1.section == valid_section and
+            seat1.row == valid_row and
+            seat1.seat_no == valid_seatno)
 
-def test_badsection:
+def test_badsection():
     #
     # A Seat constructor with an invalid section should contruct a null Seat
     #
-    assert seat2.section == None and
-           seat2.row == None and
-           seat2.seat_no == None
+    assert (seat2.section == None and
+            seat2.row == None and
+            seat2.seat_no == None)
 
-def test_badrow:
+def test_badrow():
     #
     # A Seat constructor with an invalid row should contruct a null Seat
     #
-    assert seat3.section == None and
-           seat3.row == None and
-           seat3.seat_no == None
+    assert (seat3.section == None and
+            seat3.row == None and
+            seat3.seat_no == None)
 
-def test_badseatno:
+def test_badseatno():
     #
     # A Seat constructor with an invalid seat_no should contruct a null Seat
     #
-    assert seat4.section == None and
-           seat4.row == None and
-           seat4.seat_no == None
+    assert (seat4.section == None and
+            seat4.row == None and
+            seat4.seat_no == None)
