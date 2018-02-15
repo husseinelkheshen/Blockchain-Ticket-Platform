8a47b4ae8ddefa02bd7ce3d8b9855969e0e344c1
Euirim Choi <euirim@me.com>
2018-02-15 15:58:26 -0600

    Implement login view

---
+++ request/globals/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.models import User
+
+# Create your views here.
+def login(request):
+    if request.method == "POST":
+        user = authenticate(
+            username=request.POST["username"], 
+            password=request.POST["password"])
+
+        if user and user.is_active:
+            login(request, user) 
+            return redirect("home") # redirect to homepage
+        else: # login failed
+            messages.error(request, "Invalid login credentials.")
+            return redirect("")
+    else: # display login page
+        render(request, "login.html")
