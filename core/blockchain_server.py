2283b6b02da47146ef6c30cfeaa2354bad1b82ff
Euirim Choi <euirim@me.com>
2018-02-14 20:57:44 -0600

    Readd core to fix commit user

---
+++ core/blockchain_server.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class Ticket(Resource):
+    def get(self, ticket_id):
+        return {"get ticket": ticket_id}
+
+
+class TicketList(Resource):
+    def get(self):
+        return {"list tickets": "TODO"}
+
+
+class TicketCreate(Resource):
+    def put(self):
+        return {"put ticket": "TODO"}
+
+
+api.add_resource(TicketList, "/tickets")
+api.add_resource(TicketCreate, "/tickets/create")
+api.add_resource(Ticket, "/tickets/<ticket_id>")
+
+if __name__ == "__main__":
+    app.run(debug=True)
