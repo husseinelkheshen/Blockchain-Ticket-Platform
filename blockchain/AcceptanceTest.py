1b208508e4428ade75a083c7053d0592da80bcb2
Ross Piper <rdpiper@uchicago.edu>
2018-02-15 16:54:56 -0600

    Making progress on Acceptance Test

--- blockchain/AcceptanceTest.py
+++ blockchain/AcceptanceTest.py
@@ -6,40 +6,16 @@ import copy
 valid_venue = False
 more_venues = True
 
-def make_venue():
-    """ Makes a new venue """
-    valid_venue = False
-    while not valid_venue:
-        # create a new venue
-        print('Let\'s make a new venue!')
-        name = input('Venue name (ex. Apollo Theater): ')
-        loc = input('Venue location (ex. Chicago, IL): ')
-        venue1 = Venue(name, loc)
+def load_venues():
+    """ Load up the dummy venues """
+    Venue('Adrienne Arsht Center', 'Miami, FL')
+    Venue('American Airlines Arena', 'Miami, FL')
+    Venue('Apollo Theater', 'Chicago, IL')
+    Venue('Civic Opera House', 'Chicago, IL')
+    Venue('Madison Square Garden', 'New York, NY')
 
-        # check if valid venue
-        valid_venue = venue1.id is not None
-        if not valid_venue:
-            print('Oh no! You entered invalid parameters for your new venue.')
-            print('Let\'s try that again...\n')
-
-    print('Great! You\'ve successfully created ' + name + ' in ' + loc + '.\n')
-
-def make_event(venue):
-    """ Makes a new event at the venue """
-    name = input('Event name: ')
-    desc = input('Brief event description (optional): ')
-    date = input('Date (MM/DD/YYYY): ')
-    time = input('Time (24-hour clock, HH:MM): ')
-    dt = datetime.strptime('{}, {}'.format(date, time), '%m/%d/%Y, %H:%M')
-
-
-def main():
-    """ Main method """
-    more_venues = True
-    while more_venues:
-        make_venue()
-        yn = input('Do you want to create another venue? (Y/N): ')
-        more_venues = (yn.upper() == 'Y')
+def list_venues():
+    """ List venues and return a list of tuples """
     print('\nHere is a list of all registered venues')
     print('\nID\tName')
     venue_list = []
@@ -50,17 +26,45 @@ def main():
     venue_list.sort()
     for vtup in venue_list:
         print(str(vtup[0]) + '\t' + vtup[1] + ' (' + vtup[2] + ')')
-    print('\nNow let\'s create a new event.')
+
+    return venue_list
+
+def select_venue(venue_list):
+    """
+    Select a venue from the list of venues and return
+    its index in venue_list
+    """
+    print('\nSelect a Venue to explore its events.')
     venue_ids = [i[0] for i in venue_list]
     valid_id = False
     while not valid_id:
-        print('At which venue do you want to create the event?')
         venue_id = int(input('Enter venue id: '))
         valid_id = venue_id in venue_ids
         if not valid_id:
             print('\nThat\'s not a valid venue id, let\'s try again...')
     v_index = [y[0] for y in venue_list].index(venue_id)
-    print('\nExcellent! Let\'s make a new event in ' + venue_list[v_index][2] +
-          ' at ' + venue_list[v_index][1] + '.')
+    print('\nExcellent! Let\'s take a look at the events currently listed in ' +
+          venue_list[v_index][2] + ' at ' + venue_list[v_index][1] + '.')
 
-if __name__ == '__main__': make_event(None)
+    return v_index
+
+def load_events(venue):
+    """ Loading events into the selected venue """
+    dt1 = datetime.now() + timedelta(days=7)
+    dt2 = datetime.now() + timedelta(days=23)
+    event1 = Event("Lady Gaga", dt1, "World Tour")
+    event2 = Event("Hamilton", dt2, "2016 Tony Award Winner")
+    event1.venue = venue
+    event2.venue = venue
+    venue.events[event1.id] = (event1, copy.deepcopy(event1.blockchain))
+    venue.events[event2.id] = (event2, copy.deepcopy(event2.blockchain))
+
+def main():
+    """ Main method """
+    load_venues()
+    venue_list = list_venues()
+    venue_index = select_venue(venue_list)
+    venue = venue_list[venue_index][3]
+    load_events(venue)
+
+if __name__ == '__main__': main()
