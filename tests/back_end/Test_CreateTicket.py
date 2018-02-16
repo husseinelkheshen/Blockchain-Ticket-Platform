c31f3bb4c85701752a21c0cb86b17dde79929f17
Hussein El Kheshen <husseinelkheshen@gmail.com>
2018-02-15 21:26:31 -0600

    More comprehensive Create Ticket functions

--- tests/back_end/Test_CreateTicket.py
+++ tests/back_end/Test_CreateTicket.py
@@ -8,11 +8,14 @@ venue1 = Venue("Wrigley Field", "Chicago, IL")
 event1 = Event("Lady Gaga", valid_datetime, "Stadium world tour")
 event1.venue = venue1
 seat1 = Seat("Floor Center", "G", 23)
+seat2 = Seat("Upper Center", "H", 58)
 
 venue1.events[event1.id] = (event1, copy.deepcopy(event1.blockchain))
 
 venue1.createTicket(venue1.events[event1.id][0], 550, seat1)    # success
 venue1.createTicket(venue1.events[event1.id][0], 365, seat1)    # failure
+venue1.createTicket(venue1.events[event1.id][0], 365, None)     # failure
+venue1.createTicket(venue1.events[event1.id][0], -5, seat2)     # failure
 
 most_recent_event_block = venue1.events[event1.id][0].blockchain.blocks[-1]
 most_recent_venue_block = venue1.events[event1.id][1].blocks[-1]
@@ -23,6 +26,18 @@ def test_cloning():
 
 def test_duplicate():
     """ Make sure that the duplicate Ticket is not posted """
+    assert len(most_recent_event_block.data) == 1
+
+def test_noSeat():
+    """ Make sure that the Ticket with no Seat is not posted """
+    assert len(most_recent_event_block.data) == 1
+
+def test_negativeValue():
+    """ Make sure that the Ticket with negative value is not posted """
+    assert len(most_recent_event_block.data) == 1
+
+def test_noValue():
+    """ Make sure that the Ticket with no value is not posted  """
     assert len(most_recent_event_block.data) == 1
 
 def test_listing():
