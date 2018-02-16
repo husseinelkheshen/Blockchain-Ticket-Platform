24a2da1876d8be6303d6b135ee8db27d2153b511
Euirim Choi <euirim@me.com>
2018-02-15 21:40:17 -0600

    Fix venue url bug

--- request/venues/urls.py
+++ request/venues/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("list/<int:event_id>/<int:ticket_num>/", views.buy_ticket, name="list_ticket")
+    path("list/<int:event_id>/<int:ticket_num>/", views.list_ticket, name="list_ticket")
 ]
