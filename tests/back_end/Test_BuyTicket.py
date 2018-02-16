777401c1846a514092cdcab97c6e27aa376931da
Hussein El Kheshen <husseinelkheshen@gmail.com>
2018-02-15 23:27:38 -0600

    Formatted BuyTicket Tests

--- tests/back_end/Test_BuyTicket.py
+++ tests/back_end/Test_BuyTicket.py
@@ -7,6 +7,7 @@ import copy
 # Creation of Peripheral Objects
 #
 testDatetime1 = date.datetime.now() + date.timedelta(days=7) # one week from now
+testDatetime2 = date.datetime.now() - date.timedelta(days=7) # one week ago
 
 testSeat1 = Seat("Cheap Seats", "C", 5)
 testSeat2 = Seat("Steak Sauce", "A", 1)
@@ -43,12 +44,12 @@ testEvent1.venue = testVenue
 testEvent1.id = 2
 testVenue.events[testEvent1.id] = (testEvent1, copy.deepcopy(testEvent1.blockchain))
 
-'''
-testEvent2 = Event("Class2", testDatetime2, "Tuesday and Thursday")
+
+testEvent2 = Event("Class2", testDatetime1, "Tuesday and Thursday")
 testEvent2.venue = testVenue
 testEvent2.id = 3
 testVenue.events[testEvent2.id] = (testEvent2, copy.deepcopy(testEvent2.blockchain))
-'''
+
 
 #
 # Creation of Test Tickets
@@ -72,41 +73,45 @@ testTicket6 = testVenue.createTicket(testVenue.events[testEvent1.id][0], 0, test
 testTicket6.listTicket(testTicket6.face_value, testVenue.id)
 
 
-#
-# Test purchasing of tickets with funds in wallet
-#
-def test_buyTicket_funds1():
+def test_buyTicket_EmptiesWallet():
+    """
+    Test to make sure a purchase with valid parameters and funds goes through
+    Tests edge case where wallet is emptied
+    """
     assert testUser1.buyTicket(testTicket1)
     assert testUser1.inventory[-1] == testTicket1
     assert not testTicket1.for_sale
     assert testUser1.wallet == 0
 
 
-def test_buyTicket_funds2():
+def test_buyTicket_FreeTicket():
+    """ Test to make sure a purchase where value is 0 goes through """
     assert testUser4.buyTicket(testTicket4)
     assert testUser4.inventory[-1] == testTicket4
     assert not testTicket4.for_sale
     assert testUser4.wallet == 0
 
 
-def test_buyTicket_funds3():
+def test_buyTicket_insufficientFunds():
+    """ Test to make sure a purchase where buyer has insufficient funds fails """
     assert not testUser4.buyTicket(testTicket3)
     assert testTicket3 not in testUser4.inventory
     assert testTicket3.for_sale
     assert testUser4.wallet == 0
 
 
-def test_buyTicket_funds4():
+def test_buyTicket_Valid_Paramaters():
+    """ Test to make sure a purchase with valid parameters and funds goes through """
     assert testUser3.buyTicket(testTicket3)
     assert testTicket3 in testUser3.inventory
     assert not testTicket3.for_sale
     assert testUser3.wallet == 950
 
-
-#
-# Test purchasing of tickets based on availability
-#
-def test_buyTicket_availability1():
+def test_buyTicket_AlreadySold():
+    """
+    Test to make sure a ticket which has already been sold can't be resold
+    unless the owner marks it for sale
+    """
     assert not testUser2.buyTicket(testTicket1)
     assert testTicket1 in testUser1.inventory
     assert testTicket1 not in testUser2.inventory
@@ -114,8 +119,10 @@ def test_buyTicket_availability1():
     assert testUser2.wallet == 500
 
 
-def test_buyTicket_availability2():
+def test_buyTicket_NotForSale():
+    """ Test to make sure you can't purchase a ticket not for sale """
     assert not testUser2.buyTicket(testTicket2)
     assert testTicket2 not in testUser2.inventory
     assert not testTicket1.for_sale
     assert testUser2.wallet == 500
+
