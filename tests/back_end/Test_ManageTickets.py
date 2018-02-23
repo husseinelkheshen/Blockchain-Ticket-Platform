9213a9e12f71d4cfb8648a560f68b8aa2141b37f
Hussein El Kheshen <32780368+husseinelkheshen@users.noreply.github.com>
2018-02-22 21:11:55 -0600

    Update Test_ManageTickets.py

--- tests/back_end/Test_ManageTickets.py
+++ tests/back_end/Test_ManageTickets.py
@@ -16,45 +16,45 @@ ticket2 = Ticket(testEvent1, 70, testSeat2)
 ticket3 = Ticket(testEvent1, 90, testSeat3)
 
 def test_no_event():
-    """ You can't manage Tickets if the event is not specified """
-    assert (not venue1.manageTickets(None, 80, "Cheap Seats", "C", 5) and
+    """ Tests a ManageTicket call with no Event to check for failure """
+    assert (venue1.manageTickets(None, 80, "Cheap Seats", "C", 5) is False and
             ticket1.list_price == 50)
 
 def test_no_price():
-    """ TODO """
-    assert (not venue1.manageTickets(event1, None, "Cheap Seats", "C", 5) and
+    """ Tests a ManageTicket call with no new price to check for failure """
+    assert (venue1.manageTickets(event1, None, "Cheap Seats", "C", 5) is False and
             ticket1.list_price == 50)
 
 def test_invalid_price():
-    """ TODO """
-    assert (not venue1.manageTickets(event1, -30, "Cheap Seats", "C", 5) and
+    """ Tests a ManageTicket call with an invalid price to check for failure """
+    assert (venue1.manageTickets(event1, -30, "Cheap Seats", "C", 5) is False and
             ticket1.list_price == 50)
 
 def test_invalid_row():
-    """ TODO """
-    assert (not venue1.manageTickets(event1, 80, "Cheap Seats", None, 5) and
+    """ Tests a ManageTicket call where seat is specified but row is not to check for failure """
+    assert (venue1.manageTickets(event1, 80, "Cheap Seats", None, 5) is False and
             ticket1.list_price == 50)
 
 def test_invalid_section():
-    """ TODO """
-    assert (not venue1.manageTickets(event1, 80, None, "C", 5) and
+    """ Tests a ManageTicket call where row is specified but section is not to check for failure """
+    assert (venue1.manageTickets(event1, 80, None, "C", 5) is False and
             ticket1.list_price == 50)
 
 
 def test_valid_seat():
-    """ TODO """
-    assert (venue1.manageTickets(event1, 80, "Cheap Seats", "C", 5) and
+    """ Tests a ManageTicket call with correct parameters for changing the price of one ticket"""
+    assert (venue1.manageTickets(event1, 80, "Cheap Seats", "C", 5) is True and
             ticket1.list_price == 80)
 
 def test_valid_row():
-    """ TODO """
-    assert (venue1.manageTickets(event1, 100, "Cheap Seats", "C", None) and
+    """ Tests a ManageTicket call with correct parameters for changing the price of one row of tickets"""
+    assert (venue1.manageTickets(event1, 100, "Cheap Seats", "C", None) is True and
             ticket1.list_price == 100 and
             ticket3.list_price == 100)
 
 def test_valid_section():
-    """ TODO """
-    assert (venue1.manageTickets(event1, 120, "Cheap Seats", None, None) and
+    """ Tests a ManageTicket call with correct parameters for changing the price of one section of tickets"""
+    assert (venue1.manageTickets(event1, 120, "Cheap Seats", None, None) is True and
             ticket1.list_price == 120 and
             ticket2.list_price == 120 and
             ticket3.list_price == 120)
