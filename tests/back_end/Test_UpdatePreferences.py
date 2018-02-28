e3984861e9a750c587ba8e8b3e5fead51d0add5e
ginayu <ginayu@uchicago.edu>
2018-02-28 14:31:04 -0600

    adding tests

--- tests/back_end/Test_UpdatePreferences.py
+++ tests/back_end/Test_UpdatePreferences.py
@@ -17,28 +17,54 @@ user1 = User("Gina", "Yu", "gy@example.com")
 """
 Test updatePreference's success both when called alone
 and when called in function
-
 """
+
 def test_locationpref_insertion():
+""" Test insertion of ticket location into location_pref dictionary """
 	user1.updatePreferences(ticket1, "buy", None)
 	assert(venue1.location in user1.location_pref)
 
 def test_venuepref_insertion():
+""" Test insertion of ticket venue into venue_pref dictionary """
 	user1.updatePreferences(ticket1, "buy", None)
 	assert(venue1 in user1.venue_pref)
 
 def test_descriptionpref_insertion():
+""" Test insertion of tags list into description_pref dictionary """
 
 
 def test_buyTicket_insertion():
+""" Test insertion of preferences when user purchases ticket """
 	user1.buyTicket(ticket1)
 	assert(venue1 in user1.venue_pref and venue1.location in user1.location_pref)
 
 def test_upgradeTicket_insertion():
+""" Test insertion of preferences when user upgrades ticket """
 	user1.upgradeTicket(ticket1)
 	assert(venue1 in user1.venue_pref and venue1.location in user1.location_pref)
 
 def test_search_insertion():
+""" Test insertion of preferences when user searches """
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
