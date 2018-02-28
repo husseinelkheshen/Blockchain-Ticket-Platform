4710abc6f82b4c4edc0a424ee987ca56584fcf03
ginayu <ginayu@uchicago.edu>
2018-02-28 14:23:08 -0600

    adding tests

---
+++ tests/back_end/Test_UpdatePreferences.py
@@ -0,0 +1,45 @@
+from blockchain.Admit01_Blockchain import *
+from datetime import datetime
+from datetime import timedelta
+import copy
+
+
+valid_datetime = datetime.now() + timedelta(days=7) # one week from now
+venue1 = Venue("Wrigley Field", "Chicago, IL")
+event1 = Event("Lady Gaga", valid_datetime, "Stadium world tour")
+event1.venue = venue1
+seat1 = Seat("Floor Center", "G", 23)
+venue1.events[event1.id] = (event1, copy.deepcopy(event1.blockchain))
+ticket1 = venue1.createTicket(venue1.events[event1.id][0], 550, seat1)    # success
+
+user1 = User("Gina", "Yu", "gy@example.com") 
+
+"""
+Test updatePreference's success both when called alone
+and when called in function
+
+"""
+def test_locationpref_insertion():
+	user1.updatePreferences(ticket1, "buy", None)
+	assert(venue1.location in user1.location_pref)
+
+def test_venuepref_insertion():
+	user1.updatePreferences(ticket1, "buy", None)
+	assert(venue1 in user1.venue_pref)
+
+def test_descriptionpref_insertion():
+
+
+def test_buyTicket_insertion():
+	user1.buyTicket(ticket1)
+	assert(venue1 in user1.venue_pref and venue1.location in user1.location_pref)
+
+def test_upgradeTicket_insertion():
+	user1.upgradeTicket(ticket1)
+	assert(venue1 in user1.venue_pref and venue1.location in user1.location_pref)
+
+def test_search_insertion():
+
+
+
+
