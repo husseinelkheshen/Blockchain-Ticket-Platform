f8f9c719a0a930c0ffe9c8f383f830d538661d2a
Hussein El Kheshen <husseinelkheshen@gmail.com>
2018-02-28 23:19:47 -0600

    Fixed tests

--- tests/back_end/Test_VenueTickets.py
+++ tests/back_end/Test_VenueTickets.py
@@ -4,6 +4,7 @@ from datetime import timedelta
 valid_date = date.datetime.now() + timedelta(days = 7)
 
 testSeat1 = Seat("Cheap Seats", "C", 5)
+testSeat2 = Seat("Cheap Seats", "C", 6)
 
 venue1 = Venue("Wrigley Field", "Chicago, IL")
 venue1.createEvent("Lady Gaga", valid_date, "Stadium world tour")
@@ -25,8 +26,7 @@ def test_no_tickets():
     """ Tests an Event with no tickets """
     assert venue1.venueTickets(0) is None
 
-venue1.createTicket(venue1.events[0][0], 50, testSeat1)
-
 def test_one_ticket():
     """ Tests an Event with one ticket """
-    assert venue1.venueTickets(0) is not None
+    venue1.createTicket(venue1.events[0][0], 50, testSeat1)
+    assert venue1.venueTickets(0) == [0]
