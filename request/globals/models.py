527ac8b3abba35a72720a9cbfa92ee0ddaaab98c
Euirim Choi <euirim@mewtwo.cs.uchicago.edu>
2018-03-01 19:18:03 -0600

    Add user manager

--- request/globals/models.py
+++ request/globals/models.py
@@ -1,11 +1,33 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.utils.translation import ugettext_lazy as _
 
 # Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, email, password, first_name=None, last_name=None, **extra_fields):
+        if not email:
+            raise ValueError("Enter an email address.")
+        if not password:
+            raise ValueError("Enter a password.")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, first_name=first_name, last_name=last_name, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password=password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
 class User(AbstractUser):
     username = None
     email = models.EmailField(_("email address"), unique=True)
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["email"]
+    REQUIRED_FIELDS = []
+
+    objects = UserManager
