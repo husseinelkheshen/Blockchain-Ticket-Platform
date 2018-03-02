58bf04f0ec9da8b4a51390bb2c6fb52d377d4635
Euirim Choi <euirim@mewtwo.cs.uchicago.edu>
2018-03-01 19:46:58 -0600

    Create event model

--- request/events/models.py
+++ request/events/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+from venues.models import Venue
+
 # Create your models here.
+class Event(models.Model):
+    name = models.CharField(max_length=100)
+    venue = models.ForeignKey(Venue)
+
+    def __str__(self):
+        return self.name
