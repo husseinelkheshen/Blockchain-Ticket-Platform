78cd5a8ecee13f7e3bf2bb98c13445831d098f94
Euirim Choi <euirim@mewtwo.cs.uchicago.edu>
2018-03-01 18:49:31 -0600

    Create register form

--- request/globals/forms.py
+++ request/globals/forms.py
@@ -1,5 +1,13 @@
 from django import forms
 
+USER_TYPE_CHOICES = (
+    ("customer": "Customer"),
+    ("venue": "Venue")
+)
+
 class RegisterForm(forms.Form):
-    username = forms.CharField(max_length=150)
-    
+    email = forms.EmailField()
+    password1 = forms.CharField(widget=forms.PasswordInput)
+    password2 = forms.CharField(widget=forms.PasswordInput)
+
+    user_type = forms.ChoiceField(choices=USER_TYPE_CHOICES)
