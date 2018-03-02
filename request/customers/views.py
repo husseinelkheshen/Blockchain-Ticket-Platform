0c773b41b572ae64ed84b376a593c664f25a97db
euirim <euirim@me.com>
2018-03-02 00:57:36 -0600

    Fix import error

--- request/customers/views.py
+++ request/customers/views.py
@@ -1,5 +1,5 @@
 from django.contrib import messages
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 
 from globals import blockchain_api as bcAPI
