69975f4acfc3472401897e9a8f0c1818f2bee97c
euirim <euirim@me.com>
2018-03-02 04:46:33 -0600

    Making templates personalized for venu and user

--- request/customers/urls.py
+++ request/customers/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("buy/<int:event_id>/<int:ticket_num>", views.buy_ticket, name="buy_ticket")
+    path("tickets/", views.list_customer_tickets, name="list-customer-tickets"),
+    path("buy/<int:event_id>/<int:ticket_num>", views.buy_ticket, name="buy-ticket")
 ]
