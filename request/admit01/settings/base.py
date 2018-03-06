50dc144eb9826bfc00ad8585b8caba02a72257b8
Euirim Choi <euirim@me.com>
2018-03-06 01:19:58 -0600

    Implement wallet

--- request/admit01/settings/base.py
+++ request/admit01/settings/base.py
@@ -70,6 +70,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'globals.context_processors.user_type',
+                'globals.context_processors.wallet'
             ],
         },
     },
--- request/globals/context_processors.py
+++ request/globals/context_processors.py
@@ -1,5 +1,6 @@
 from venues.models import Venue
 from customers.models import Customer
+from globals import blockchain_api as bcAPI
 
 def user_type(request):
     context = {
@@ -19,6 +20,51 @@ def user_type(request):
             customer = Customer.objects.get(user=request.user)
             context["userType"] = "customer"
             context["userClass"] = customer
+        except Customer.DoesNotExist:
+            pass
+    
+    return context
+
+def wallet(request):
+    context = {
+        "balance": None
+    }
+
+    if not request.user.is_authenticated:
+        return context
+
+    try:
+        venue = Venue.objects.get(user=request.user)
+        
+        data = {
+            "venue": {
+                "venue_location": venue.location,
+                "venue_name": venue.name,
+            }
+        }
+
+        response = bcAPI.post('venue/view_wallet', data=data)
+
+        if response[1] == 200:
+            context["balance"] = response[0]["wallet"]
+        else:
+            return context
+
+    except Venue.DoesNotExist:
+        try: 
+            customer = Customer.objects.get(user=request.user)
+
+            data = {
+                "user_email": customer.user.email
+            }
+
+            response = bcAPI.post('user/view_wallet', data=data)
+
+            if response[1] == 200:
+                context["balance"] = response[0]["wallet"]
+            else:
+                return context
+
         except Customer.DoesNotExist:
             pass
     
