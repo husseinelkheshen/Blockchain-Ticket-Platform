9faf2fb594c9e5e1d37d36f43d3ae96f1cf46f16
Ubuntu <ubuntu@ip-172-31-17-77.us-east-2.compute.internal>
2018-03-06 05:47:47 +0000

    Begin flexible settings

--- request/manage.py
+++ request/manage.py
@@ -2,8 +2,13 @@
 import os
 import sys
 
+if not os.environ["ADMIT01_ENV_TYPE"]:
+    env = "dev"
+else:
+    env = "prod"
+
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "admit01.settings.dev")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "admit01.settings." + env)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
