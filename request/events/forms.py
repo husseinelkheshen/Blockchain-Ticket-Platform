518582b21f06fae3617e27a3af24afbd6b308c6d
Euirim Choi <euirim@me.com>
2018-03-07 01:22:15 -0600

    Fix form label and validation

--- request/events/forms.py
+++ request/events/forms.py
@@ -16,6 +16,24 @@ class CreateTicketsForm(forms.Form):
     max_seat= forms.IntegerField(min_value=0)
     face_value = forms.FloatField(min_value=0)
 
+    def clean(self):
+        cleaned_data = super().clean()
+        min_row = cleaned_data.get("min_row")
+        max_row = cleaned_data.get("max_row")
+        min_seat = cleaned_data.get("min_seat")
+        max_seat = cleaned_data.get("max_seat")
+
+        if min_row > max_row:
+            raise forms.ValidationError(
+                "Min row must be less than max row."
+            )
+
+        if min_seat > max_seat:
+            raise forms.ValidationError(
+                "Min seat must be less than max seat."
+            )
+
+
 class EditEventForm(forms.ModelForm):
     class Meta:
         model = Event
@@ -25,4 +43,4 @@ class EditTicketsForm(forms.Form):
     section = forms.CharField(max_length=100)
     row = forms.IntegerField(required=False)
     seat = forms.IntegerField(required=False)
-    face_value = forms.FloatField(required=False)
+    face_value = forms.FloatField(required=False, label="List price")
